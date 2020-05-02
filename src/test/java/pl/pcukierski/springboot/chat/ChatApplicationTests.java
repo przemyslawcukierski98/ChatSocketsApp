0cb2e43c42ab55e9e41f760b4f7c845ee2928cd6
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2020-05-02 08:46:17 +0200

    Initial commit

---
+++ src/test/java/pl/pcukierski/springboot/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pcukierski.springboot.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
