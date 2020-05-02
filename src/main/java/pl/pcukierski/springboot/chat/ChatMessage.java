93c01989c2e673fc9591fe2148e91ae1549e2eb5
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2020-05-02 09:02:56 +0200

    Create simple GUI
    Simple GUI with HTML, add ChatMessage.java class

---
+++ src/main/java/pl/pcukierski/springboot/chat/ChatMessage.java
@@ -0,0 +1,18 @@
+package pl.pcukierski.springboot.chat;
+
+public class ChatMessage {
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public ChatMessage(){
+
+    }
+}
