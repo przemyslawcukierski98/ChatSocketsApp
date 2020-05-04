915649ac01847285a6139b5afc4f6918be909c19
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2020-05-04 15:54:09 +0200

    Info about user
    Add info about user to project

--- src/main/java/pl/pcukierski/springboot/chat/ChatMessage.java
+++ src/main/java/pl/pcukierski/springboot/chat/ChatMessage.java
@@ -4,12 +4,22 @@ public class ChatMessage {
 
     private String value;
 
+    private String user;
+
     public String getValue() {
         return value;
     }
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
     }
 
     public ChatMessage(){
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,9 +1,10 @@
 var client = null;
 
-function showMessage(value){
-    var response = document.getElementById('response');
+function showMessage(value, user){
     var newResponse = document.createElement('p');
+    newResponse.appendChild(document.createTextNode(user));
     newResponse.appendChild(document.createTextNode(value));
+    var response = document.getElementById('response');
     response.appendChild(newResponse);
 }
 
@@ -11,12 +12,13 @@ function connect(){
     client = Stomp.client('ws:localhost:8080/chat');
     client.connect({}, function(frame){
         client.subscribe("/topic/messages", function(message){
-            showMessage(JSON.parse(message).value);
+            showMessage(JSON.parse(message.body).value, JSON.parse(message.body).user);
         });
     })
 }
 
 function sendMessage(){
     var messageInput = document.getElementById('messageInput').value;
-    client.send("/app/chat", {}, JSON.stringify({'value': messageInput}));
+    var user = document.getElementById('user').value;
+    client.send("/app/chat", {}, JSON.stringify({'value': messageInput, 'user': user}));
 }
